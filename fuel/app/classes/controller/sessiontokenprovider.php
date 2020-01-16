3f901c34168f091ffb8c5b2d954f6964370740a8
NGUYỄN THANH VIỆT <nguyenthanhviet31031998@gmail.com>
2020-01-16 11:50:23 +0700

    [UPDATE] Update generate_session_key method in SessionTokenProvider class.

--- fuel/app/classes/controller/sessiontokenprovider.php
+++ fuel/app/classes/controller/sessiontokenprovider.php
@@ -1,35 +1,38 @@
 <?php
 include 'randomuuid.php';
-include 'randomtoken.php';
 include 'sessiontokenrepository.php';
 
 use \Firebase\JWT\JWT;
-use Ramsey\Uuid\Uuid;
-use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
 
 class SessionTokenProvider
 {
 
     public function generate_session_key($user_id, $user_email, $user_role)
     {
-        $session_key = new RandomUUID();
-        $key = "secret";
-        $now = date('Y-m-d H:i:s', time());
-        $iat = strtotime($now);
-        $exp = $iat +  1579663275;
+        // gen uuid
+        $randome_uuid = new RandomUUID();
+        $session_key = $randome_uuid->gen_uuid();
+
+        // gen token
+        $key_secret = "secret";
+        $issued_date = time();
+        $expiration_date = $issued_date + 604800; // Plus 1 week
         $payload = array(
             "iss" => "vietdeptrai",
-            "iat" => $iat,
-            "exp" => $exp,
+            "iat" => $issued_date,
+            "exp" => $expiration_date,
             "data" => array(
                 "id" => $user_id,
                 "email" => $user_email,
                 "role" => $user_role
             )
         );
-        $token = JWT::encode($payload, $key);
-        $session_token = new SessionTokenRepository();
-        $session_token->save_session_token($session_key->gen_uuid(), $token);
-        return $token;
+        $token = JWT::encode($payload, $key_secret);
+        if($session_key === null or $token === null) Log::error('$session_key = null or token = null');
+
+        // save session_key and token into database
+        $session_token_repository = new SessionTokenRepository();
+        $session_token_repository->save_session_token($session_key, $token);
+        return $session_key;
     }
 }
